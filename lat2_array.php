28b6c93052acf6c2c9a814a18b5a994155ee8f81
agdd1709 <53288058+agdd1709@users.noreply.github.com>
2021-04-17 16:15:55 +0700

    input data array 2

--- lat2_array.php
+++ lat2_array.php
@@ -1,3 +1,11 @@
 <?php
 $angka = array(6, 7, 8, 5, 3);
 var_dump($angka);
+
+$ibukota = array(
+    "jabar" => "Bandung",
+    "jateng" => "Semarang",
+    "jatim" => "Surabaya",
+    "bali" => "Denpasar",
+    "banten" => "Serang"
+);
