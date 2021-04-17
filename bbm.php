302a1c210686ac86025f7290ad90b97cb5af86be
agdd1709 <53288058+agdd1709@users.noreply.github.com>
2021-04-17 14:56:56 +0700

    tamabah style

--- bbm.php
+++ bbm.php
@@ -6,6 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Daftar Harga BBM</title>
+    <style>
+        td {
+            text-align: right;
+        }
+    </style>
 </head>
 
 <body>
@@ -25,35 +30,40 @@
     </form>
     <hr>
     <?php
+    if (isset($_POST["tblsubmit"])) {
     ?>
-    <table border="1px">
-        <tr>
-            <th>Liter</th>
-            <th>Premium</th>
-            <th>Pertalite</th>
-            <th>Pertamax</th>
-            <th>Solar</th>
-        </tr>
-        <?php
-        define("HARGA_PREMIUM", 6550);
-        define("HARGA_PERTALITE", 7600);
-        define("HARGA_PERTAMAX", 8900);
-        define("HARGA_SOLAR", 5150);
-        $awal = $_POST["awal"];
-        $akhir = $_POST["akhir"];
-        for ($liter = $awal; $liter <= $akhir; $liter++) {
-        ?>
+        <table border="1px">
             <tr>
-                <td><?php echo $liter; ?></td>
-                <td><?php echo $liter * HARGA_PREMIUM; ?></td>
-                <td><?php echo $liter * HARGA_PERTALITE; ?></td>
-                <td><?php echo $liter * HARGA_PERTAMAX; ?></td>
-                <td><?php echo $liter * HARGA_SOLAR; ?></td>
+                <th>Liter</th>
+                <th>Premium</th>
+                <th>Pertalite</th>
+                <th>Pertamax</th>
+                <th>Solar</th>
             </tr>
-        <?php
-        }
-        ?>
-    </table>
+            <?php
+            define("HARGA_PREMIUM", 6550);
+            define("HARGA_PERTALITE", 7600);
+            define("HARGA_PERTAMAX", 8900);
+            define("HARGA_SOLAR", 5150);
+            $awal = $_POST["awal"];
+            $akhir = $_POST["akhir"];
+            for ($liter = $awal; $liter <= $akhir; $liter++) {
+            ?>
+                <tr>
+                    <td><?php echo $liter; ?></td>
+                    <td>Rp <?php echo number_format($liter * HARGA_PREMIUM, 0, ",", "."); ?></td>
+                    <td>Rp <?php echo number_format($liter * HARGA_PERTALITE, 0, ",", "."); ?></td>
+                    <td>Rp <?php echo number_format($liter * HARGA_PERTAMAX, 0, ",", "."); ?></td>
+                    <td>Rp <?php echo number_format($liter * HARGA_SOLAR, 0, ",", "."); ?></td>
+                </tr>
+            <?php
+            }
+            ?>
+        </table>
+    <?php
+    } else
+        echo  "Isilah Liter awal dan liter akhir terlebih dahulu";
+    ?>
 </body>
 
 </html>
