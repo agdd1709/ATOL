75147004ea7be15979126126e4d901cb8cc0e570
agdd1709 <53288058+agdd1709@users.noreply.github.com>
2021-04-17 13:50:09 +0700

    awl

---
+++ bbm.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Harga BBM</title>
+</head>
+
+<body>
+    <form method="POST">
+        <table border="1">
+            <tr>
+                <th>Liter Awal</th>
+                <th>Liter Akhir</th>
+                <th>&nbsp;</th>
+            </tr>
+            <tr>
+                <td><input type="text" name="awal" maxlength="3" size="4"></td>
+                <td><input type="text" name="akhir" maxlength="3" size="4"></td>
+                <td><input type="submit" name="tblsubmit" maxlength="3" size="4"></td>
+            </tr>
+
+        </table>
+    </form>
+
+</body>
+
+</html>
