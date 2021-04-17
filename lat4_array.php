2ce4fd6e7d9984b64f3884332382633b0a2b85f8
agdd1709 <53288058+agdd1709@users.noreply.github.com>
2021-04-17 16:50:48 +0700

    review latihan array 4

---
+++ lat4_array.php
@@ -0,0 +1,13 @@
+<?php
+$ibukota = array(
+    "jabar" => "Bandung",
+    "jateng" => "Semarang",
+    "jatim" => "Surabaya",
+    "bali" => "Denpasar",
+    "banten" => "Serang"
+);
+echo "Hanya mengambil value saja : <br>";
+foreach ($ibukota as $kota)
+    echo  $kota . "<br>";
+
+    ?>
