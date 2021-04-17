af2ab740e40f820b46ad453599bb665a2e456b66
agdd1709 <53288058+agdd1709@users.noreply.github.com>
2021-04-17 16:02:22 +0700

    latihan1 array selesai

--- lat_array.php
+++ lat_array.php
@@ -1,7 +1,16 @@
-<?php 
-$angka[0]=6;
-$angka[1]=7;
-$angka[2]=8;
-$angka[3]=5;
-$angka[4]=3;
+<?php
+$angka[0] = 6;
+$angka[1] = 7;
+$angka[2] = 8;
+$angka[3] = 5;
+$angka[4] = 3;
 var_dump($angka);
+
+$ibukota["jabar"] = "Bandung";
+$ibukota["jateng"] = "Semarang";
+$ibukota["jatim"] = "Surabaya";
+$ibukota["bali"] = "Denpasar";
+$ibukota["banten"] = "serang";
+echo "<pre>";
+print_r($ibukota);
+echo "</pre>"
