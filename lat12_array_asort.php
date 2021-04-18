06ea0d37264fd85be68953b00951ec037b42d69b
agdd1709 <53288058+agdd1709@users.noreply.github.com>
2021-04-18 12:59:22 +0700

    review latihan array 12

--- lat12_array_asort.php
+++ lat12_array_asort.php
@@ -15,4 +15,6 @@ echo "<pre>" . print_r($ibukota, true) . "</pre>";
 ksort($ibukota);
 echo "Data telah di urutkan secara ksort";
 echo "<pre>" . print_r($ibukota, true) . "</pre>";
-?>
+krsort($ibukota);
+echo "Data telah di urutkan secara krsort";
+echo "<pre>" . print_r($ibukota, true) . "</pre>";
