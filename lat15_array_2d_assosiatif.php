98510220d6d55370cd41a2a5d952cbe68ff8159d
agdd1709 <53288058+agdd1709@users.noreply.github.com>
2021-04-18 13:14:30 +0700

    review latihan array 15

---
+++ lat15_array_2d_assosiatif.php
@@ -0,0 +1,17 @@
+<?php
+$prov = array(
+    "jabar" => array(
+        "nama" => "Jawa Barat",
+        "ibukota" => "Bandung"
+    ),
+    "jateng" => array(
+        "nama" => "Jawa tengah",
+        "ibukota" => "Semarang"
+    ),
+    "jatim" => array(
+        "nama" => "Jawa Timur",
+        "ibukota" => "Surabaya"
+    ),
+);
+echo $prov["jabar"]["ibukota"];
+?>
