0f0d3d53dc46c36df39b49db2fd46a38ab80df8e
agdd1709 <53288058+agdd1709@users.noreply.github.com>
2021-04-18 13:07:16 +0700

    review latihan array 14

---
+++ lat14_array_multidimensi.php
@@ -0,0 +1,9 @@
+<?php
+$jarak = array(
+    array(0, 0, 4, 0, 7),
+    array(0, 0, 0, 6, 0),
+    array(4, 0, 0, 0, 5),
+    array(0, 10, 0, 0, 0),
+    array(7, 0, 5, 0, 0)
+);
+echo "Jarak dari titik 1 ke titik 3 adalah " . $jarak[1][3];
