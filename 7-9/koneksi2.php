39888b7c21810632aae93061f7348d99475d4ad8
agdd1709 <53288058+agdd1709@users.noreply.github.com>
2021-06-09 05:31:39 +0700

    sukses

--- 7-9/koneksi2.php
+++ 7-9/koneksi2.php
@@ -1,9 +1,9 @@
 <?php
 include_once("function.php");
 $db = dbConnect();
-if ($db->connect_errno = 0) {
+if ($db->connect_errno == 0) {
     echo "Sukses Koneksi";
 } else {
-    echo "Gagal Koneksi ".
-    {DEVELOPMENT ? " ". $db->connect_error : ""} . "<br>";
+    echo "Gagal Koneksi " .
+        (DEVELOPMENT ?  $db->connect_error : "") . "<br>";
 }
